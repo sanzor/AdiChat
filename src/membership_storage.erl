662b1e5f896d43f34424ab62e39f2372636d228a
Adrian Bercovici <adriansan_93@yahoo.com>
2023-05-23 22:34:01 +0300

    adding postgres

--- src/membership_storage.erl
+++ src/membership_storage.erl
@@ -1,6 +1,11 @@
 -module(membership_storage).
+-export([subscribe/2,unsubscribe/2]).
 
 
--spec subscribe(Topic,User) -> ok when Topic::binary(),User::binary().
+-spec subscribe(Topic::binary(),User::binary()) -> ok | already_subscribed | {error,Error::term()}.
 subscribe(Topic,User)->
+    ok.
+
+-spec unsubscribe(Topic::binary(),User::binary())-> ok | not_joined | {error,Error::term()}.
+unsubscribe(Topic,User)->
     ok.
