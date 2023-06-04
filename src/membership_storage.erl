05dbc8db8bab660935a571882f12ecd54e66be63
Adrian Bercovici <adriansan_93@yahoo.com>
2023-06-04 16:02:46 +0300

    added checking if its subscribed

--- src/membership_storage.erl
+++ src/membership_storage.erl
@@ -4,7 +4,7 @@
 
 -spec subscribe(Topic::binary(),User::binary()) -> ok | already_subscribed | {error,Error::term()}.
 subscribe(Topic,User)->
-    Pg=application:get_env(wsapp,pg),
+    {ok,Pg}=application:get_env(wsapp,pg),
     Hostname=proplists:get_value(hostname,Pg),
     Port=proplists:get_value(port,Pg),
     Username=proplists:get_value(username,Pg),
@@ -19,12 +19,16 @@ subscribe(Topic,User)->
         database=>Database,
         timeout=>4000
     }),
-    {ok,_}=epgsql:equery(C,Statement,[Topic,User]),
-    ok.
+    case check_if_subscribed(Topic, User) of
+        true -> already_subscribed;
+        false ->{ok,_}=epgsql:equery(C,Statement,[Topic,User]),
+                       ok   
+    end.
+
 
 -spec unsubscribe(Topic::binary(),User::binary())-> ok | not_joined | {error,Error::term()}.
 unsubscribe(Topic,User)->
-    Pg=application:get_env(wsapp,pg),
+    {ok,Pg}=application:get_env(wsapp,pg),
     Hostname=proplists:get_value(hostname,Pg),
     Port=proplists:get_value(port,Pg),
     Username=proplists:get_value(username,Pg),
@@ -43,7 +47,7 @@ unsubscribe(Topic,User)->
     ok.
 -spec get_subscriptions_for_topic(Topic::binary())-> {ok,list()} | {error,Reason::term()}.
 get_subscriptions_for_topic(Topic)->
-    Pg=application:get_env(wsapp,pg),
+    {ok,Pg}=application:get_env(wsapp,pg),
     Hostname=proplists:get_value(hostname,Pg),
     Port=proplists:get_value(port,Pg),
     Username=proplists:get_value(username,Pg),
@@ -60,3 +64,23 @@ get_subscriptions_for_topic(Topic)->
     }),
     {ok,_}=epgsql:equery(C,Statement,[Topic]),
     ok.
+
+-spec check_if_subscribed(Topic::binary(),User::binary())->{ok,boolean()}|{error,Error::term()}.
+check_if_subscribed(Topic,User)->
+    {ok,Pg}=application:get_env(wsapp,pg),
+    Hostname=proplists:get_value(hostname,Pg),
+    Port=proplists:get_value(port,Pg),
+    Username=proplists:get_value(username,Pg),
+    Password=proplists:get_value(password,Pg),
+    Database=proplists:get_value(database,Pg),
+    Statement= <<"SELECT Count(user_id) FROM  wschat_user WHERE topic = $1 AND user_id=$2">>,
+    {ok,C}=epgsql:connect(#{
+        host=>Hostname,
+        port=>Port,
+        username=>Username,
+        password=>Password,
+        database=>Database,
+        timeout=>4000
+    }),
+    {ok,Count}=epgsql:equery(C,Statement,[Topic,User]),
+    {ok,Count>0}.
