6c7ed4fd9f495ffc50485fe16bdd1a14aed03fcb
Adrian Bercovici <adriansan_93@yahoo.com>
2023-11-04 22:07:55 +0200

    creating db works

--- start.sh
+++ start.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
-# Define the PostgreSQL username and password
+# Get the PostgreSQL username and password
 PGUSER="admin"
 PGPASSWORD="test123"
 
-# Define the SQL script path
+# Your SQL script path
 SQL_SCRIPT_PATH="/home/sanzor/NovaWebsocketServer/resources/tables.pgsql"
 
 # Define the Kubernetes context you want to use
 KUBE_CONTEXT="docker-desktop"
 
-# Change the working directory to the location of the SQL script
-cd "$(dirname "$SQL_SCRIPT_PATH")"
+# Get the current username
+CURRENT_USER=$(whoami)
 
-# Get the name of the PostgreSQL pod
+# Get the PostgreSQL pod name
 POSTGRES_POD=$(kubectl get pods -l app=postgres -o jsonpath='{.items[0].metadata.name}' --context="$KUBE_CONTEXT")
 
-# Use kubectl cp to copy the SQL script to the PostgreSQL pod
+# Change working directory to where the SQL script is located
+cd "$(dirname "$SQL_SCRIPT_PATH")"
+
+# Copy the SQL script to the PostgreSQL pod
 kubectl cp "$(basename "$SQL_SCRIPT_PATH")" "$POSTGRES_POD:$(basename "$SQL_SCRIPT_PATH")" --context="$KUBE_CONTEXT"
 
 # Change back to the original working directory
 cd -
 
-# Connect to the PostgreSQL pod and run the SQL script using sudo -S to provide the password
-echo "$PGPASSWORD" | sudo -S kubectl exec -i "$POSTGRES_POD" -- psql -U "$PGUSER" -d postgresdb -f "$(basename "$SQL_SCRIPT_PATH")"
+# Use echo to provide the password to sudo
+echo "$PGPASSWORD" | sudo -S -u "$CURRENT_USER" kubectl exec -i "$POSTGRES_POD" -- psql -U "$PGUSER" -d postgresdb -f "$(basename "$SQL_SCRIPT_PATH")"
 
 # Exit the script
 exit
