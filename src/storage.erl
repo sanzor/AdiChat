aaf9acd4ba9a438d7d8bbd322d250d0754aeacf4
Adrian Bercovici <adriansan_93@yahoo.com>
2024-08-09 21:59:36 +0300

    finished the dets storage module

--- src/storage.erl
+++ src/storage.erl
@@ -1,7 +1,6 @@
 -module(storage).
 -include("../include/domain.hrl").
 -export([
-         to_map/3,
          create_user/1,
          get_user/1,
          get_user_by_email/1,
@@ -20,261 +19,197 @@
          get_newest_messages/2,
          write_chat_message/1,
          write_chat_messages/1]).
--define(DB_SERVER_KEY,pg2).
 
+-define(USER_TABLE,"wsuser.dets").
+-define(TOPIC_TABLE,"topic.dets").
+-define(USER_TOPIC_TABLE,"user_topic.dets").
+-define(MESSAGE_TABLE,"message.dets").
 
+-spec start()->ok | {error,term()}.
+start()->
+    dets:open_file(?USER_TABLE,[]),
+    dets:open_file(?TOPIC_TABLE,[]),
+    dets:open_file(?USER_TOPIC_TABLE,[]),
+    dets:open_file(?MESSAGE_TABLE,[]).
 
-create_connection()->
-    {ok,Pg}=application:get_env(wsapp,?DB_SERVER_KEY),
-    Hostname=proplists:get_value(hostname,Pg),
-    Port=proplists:get_value(port,Pg),
-    UserName=proplists:get_value(username,Pg),
-    Password=proplists:get_value(password,Pg),
-    Database=proplists:get_value(database,Pg),
-    {ok,C}=epgsql:connect(#{
-        host=>Hostname,
-        port=>Port,
-        username=>UserName,
-        password=>Password,
-        database=>Database,
-        timeout=>4000
-    }),
-    {ok,C}.
-    
--spec get_user(UserId::domain:user_id())->{ok,User::domain:user()} | {error,Error::any()}.
-get_user(UserId)->
-    Statement= <<"Select * FROM wsuser WHERE id=$1">>,
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[UserId]),
-    Value=case to_records(Columns, Values) of
-        [] ->user_does_not_exist;
-        Else->  UserMap=lists:nth(1, Else),
-                User=utils:to_user(UserMap),
-                {ok,User}
-    end,
-    Value.
-
--spec get_user_by_email(UserId::domain:user_id())->{ok,User::domain:user()} | user_does_not_exist.
-get_user_by_email(Email)->
-    Statement= <<"Select * FROM wsuser WHERE email=$1">>,
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[Email]),
-    Value=case to_records(Columns, Values) of
-        [] ->user_does_not_exist;
-        Else->    UserMap=lists:nth(1, Else),
-                  User=utils:to_user(UserMap),
-                  {ok,User}
-    end,
-    Value.
-
-
--spec create_user(UserData::domain:create_user_params())-> {ok,User::domain:user()} | already_exists | {error,Error::any()}.
-create_user(_UserData=#create_user_params{email = Email,name = Name,password = Password})->
-        
-        Statement= <<"INSERT INTO  wsuser(email,password,name) values ($1,$2,$3) RETURNING *">>,
-        {ok,C}=create_connection(),
-        Result=case epgsql:equery(C,Statement,[Email,Password,Name]) of
-                    
-                    {error,{error,error,<<"23505">>,unique_violation,_,_}}->already_exists;
-                    {ok,_,Columns,Values}->
-                                    
-                                  Value=case to_records(Columns, Values) of
-                                            [] -> user_does_not_exist;
-                                            Else-> 
-                                                    UserMap=lists:nth(1, Else),
-                                                    User=utils:to_user(UserMap),
-                                                    {ok,User}
-                                                   
-                                        end,
-                                  Value
-                    end,
-        Result.
-
+-spec stop() -> ok | {error, term()}.
+stop()->
    
-       
-     
+    dets:close(?USER_TABLE),
+    dets:close(?TOPIC_TABLE),
+    dets:close(?USER_TOPIC_TABLE),
+    dets:close(?MESSAGE_TABLE).
 
-
-
--spec delete_user(UserId::domain:user_id())-> ok  | {error,Error::any()}.
-delete_user(UserId)->
-    try
-        Statement= <<"DELETE FROM  wsuser WHERE id = $1">>,
-        {ok,C}=create_connection(),
-        {ok,Rows}=epgsql:equery(C,Statement,[UserId]),
-        if Rows>0 -> ok ; true -> {error,user_does_not_exist} end
-    catch
-        Error:Reason -> {error,{Error,Reason}}
+-spec create_user(UserData::domain:create_user_params())->{ok,domain:user()}| already_exists | {error,term()}.
+create_user(#create_user_params{email = Email,name = Name,password=Password})->
+    case dets:match_object(?USER_TABLE, {'_','_',Email,'_'}) of
+        [] -> NewId=erlang:unique_integer([monotonic,positive]),
+              NewUser={NewId,Email,Password,Name},
+              dets:insert(?USER_TABLE, NewUser),
+              {ok,NewUser};
+        _ -> already_exists
     end.
 
-
--spec get_topic_by_name(TopicName::binary())->{ok,Topic::domain:topic()} | topic_does_not_exist | {error,Error::any()}.
-get_topic_by_name(TopicName)->
-    Statement= <<"Select * FROM topic WHERE name=$1">>,
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[TopicName]),
-    Value=case to_records(Columns, Values) of
-        [] ->topic_does_not_exist;
-        Else->  TopicMap=lists:nth(1, Else),
-                Topic=Topic=utils:to_topic(TopicMap),
-                {ok,Topic}
-    end,
-    Value.
-
--spec get_topic(TopicId::integer())->{ok,Topic::domain:topic()} | topic_does_not_exist | {error,Error::any()}.   
-get_topic(TopicId)->
-    Statement= <<"Select * FROM topic WHERE id=$1">>,
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[TopicId]),
-    Value=case to_records(Columns, Values) of
-        [] ->topic_does_not_exist;
-        Else->  TopicMap=lists:nth(1, Else),
-                Topic=Topic=utils:to_topic(TopicMap),
-                {ok,Topic}  
-    end,
-    Value.
--spec create_topic(TopicData::domain:create_topic_params())-> {ok,Topic::domain:topic()} | already_exists | {error,Error::any()}.
-create_topic(_TopicData =#create_topic_params{user_id = UserId,name = TopicName})->
-    
-        Statement= <<"INSERT INTO  topic(name,created_by) values ($1,$2) RETURNING * ">>,
-        {ok,C}=create_connection(),
-        Result=case epgsql:equery(C,Statement,[TopicName,UserId]) of
-                   
-                    {error,{error,error,<<"23505">>,unique_violation,_,_}}->already_exists;
-                    {ok,_,Columns,Values}->
-                                  Value=case to_records(Columns, Values) of
-                                  [] ->user_does_not_exist;
-                                  Else-> TopicMap=lists:nth(1, Else),
-                                         Topic=utils:to_topic(TopicMap),
-                                         {ok,Topic}
-                                  end,
-                                  Value
-                    end,
-        Result.
-   
-   
-
-
-
-
--spec delete_topic(Id::domain:topic_id())-> ok | {error,Error::any()}.
-delete_topic(Id)->
-    try
-        Statement= <<"DELETE FROM  topic WHERE id=$1">>,
-        {ok,C}=create_connection(),
-        {ok,Rows}=epgsql:equery(C,Statement,[Id]),
-        if Rows>0 -> ok ; true -> {error,topic_does_not_exist} end
-    catch
-    Error:Reason->{error,{Error,Reason}}
+-spec get_user(UserId::domain:user_id())-> {ok,domain:user()} | user_does_not_exist | {error,term()}.
+get_user(UserId)->
+    case dets:lookup(?USER_TABLE,UserId) of
+        [User]->{ok,User};
+        []-> user_does_not_exist
 end.
 
--spec subscribe(TopicId::domain:topic_id(),UserId::domain:user_id())-> ok | {error,Error::any()}.
+-spec get_user_by_email(Email::string())->{ok,domain:user()} | user_does_not_exist | {error,term()}.
+
+get_user_by_email(Email)->
+    case dets:match_object(?USER_TABLE,{'_',Email,'_','_'}) of
+        [] -> user_does_not_exist;
+        [User]-> {ok,User}
+end.
+
+-spec delete_user(UserId::domain:user_id())->ok | {error,term()}.
+delete_user(UserId)->
+    dets:delete(?USER_TABLE,UserId),
+    ok.
+
+-spec create_topic(TopicData::domain:create_topic_params())->{ok,Topic::domain:topic()}| already_exists | {error,Reason::any()}.
+create_topic(#create_topic_params{name = TopicName,user_id = UserId})->
+    case dets:match_object(?TOPIC_TABLE, {'_',TopicName,'_','_'}) of
+        []->
+            NewId=erlang:unique_integer([monotonic,positive]),
+            CreatedAt=erlang:now(),
+            Topic={NewId,TopicName,UserId,CreatedAt},
+            dets:insert(?TOPIC_TABLE, Topic),
+            {ok,Topic};
+        [_] -> already_exists
+end.
+-spec get_topic(TopicId::integer())->{ok,Topic::domain:topic()} | topic_does_not_exist | {error,Error::any()}.   
+get_topic(TopicId)->
+    case dets:lookup(?TOPIC_TABLE,TopicId) of 
+        [Topic] -> {ok,Topic};
+        _ -> topic_does_not_exist
+end.
+
+-spec get_topic_by_name(TopicName::string())->{ok,domain:topic()} | topic_does_not_exist | {error,term()}.
+
+get_topic_by_name(TopicName)->
+    case dets:match_object(?TOPIC_TABLE,{'_',TopicName,'_','_'}) of
+        [] -> user_does_not_exist;
+        [Topic]-> {ok,Topic}
+end.
+
+-spec delete_topic(Id::domain:topic_id()) -> ok | {error, term()}.
+delete_topic(Id) ->
+    dets:delete(?TOPIC_TABLE, Id),
+    ok.
+
+-spec does_topic_exist(TopicId::domain:topic_id()) -> true | false | {error, Reason::term()}.
+does_topic_exist(TopicId) ->
+    case dets:lookup(?TOPIC_TABLE, TopicId) of
+        [_] -> true;
+        [] -> false
+    end.
+
+-spec subscribe(TopicId::domain:topic_id(),UserId::domain:user_id())->ok |{error,Reason::term()}.
 subscribe(TopicId,UserId)->
-    Statement= <<"INSERT INTO  user_topic(topic_id,user_id) values ($1,$2)">>,
-    {ok,C}=create_connection(),
-    {ok,_}=epgsql:equery(C,Statement,[TopicId,UserId]),
-     ok.                 
-   
+    NewId=erlang:unique_integer([monotonic,positive]),
+    CreatedAt=erlang:now(),
+    UserTopic={NewId,UserId,TopicId,CreatedAt},
+    dets:insert(?USER_TOPIC_TABLE,UserTopic),
+    ok.
 
-
--spec unsubscribe(TopicId::domain:topic_id(),UserId::domain:user_id())-> ok | not_joined | {error,Error::term()}.
+-spec unsubscribe(TopicId::domain:topic_id(),UserId::domain:user_id())-> ok | not_joined | {error,Reason::term()}.
 unsubscribe(TopicId,UserId)->
-    Statement= <<"DELETE FROM  user_topic WHERE topic_id=$1 and user_id = $2">>,
-    {ok,C}=create_connection(),
-    {ok,Rows}=epgsql:equery(C,Statement,[TopicId,UserId]),
-    if Rows>0 -> ok ; true -> not_joined end.
-
-
-
+    case dets:match_object(?USER_TOPIC_TABLE, {'_',UserId,TopicId,'_'}) of
+        [UserTopic] -> dets:delete(?USER_TOPIC_TABLE,UserTopic),
+                       ok;
+        [] -> not_joined
+end.
 
 -spec get_subscriptions_for_topic(TopicId::domain:topic_id())-> {ok,list()} | {error,Reason::term()}.
 get_subscriptions_for_topic(TopicId)->
-    Statement= <<"SELECT user_id FROM  user_topic WHERE topic_id = $1">>,
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[TopicId]),
-    {ok,to_records(Columns, Values)}.
+    Result=[#{<<"id">>=>Id, <<"user_id">>=>UserId, <<"topic_id">>=>TopicId ,<<"created_at">>=>CreatedAt} || {Id,UserId,_,CreatedAt}<-dets:match_object(?USER_TOPIC_TABLE, {'_','_',TopicId,'_'})],
+    Result.
 
--spec get_user_subscriptions(UserId::domain:user_id())-> {ok,Subscriptions::list()}  | {error,Error::term()}.
-get_user_subscriptions(UserId)-> 
-    Statement= <<"SELECT topic.id,topic.name FROM  user_topic INNER JOIN topic on user_topic.topic_id=topic.id WHERE user_topic.user_id = $1">>,
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[UserId]),
-     {ok,to_records(Columns, Values)}.
-    
--spec check_if_subscribed(Topic::domain:topic_id(),UserId::domain:user_id())->{ok,boolean()}|{error,Error::term()}.
+-spec get_user_subscriptions(UserId::domain:user_id()) -> {ok,list()}|{error,Reason::term()}.
+get_user_subscriptions(UserId)->
+    Result=[#{<<"id">>=>Id, <<"user_id">>=>UserId, <<"topic_id">>=>TopicId ,<<"created_at">>=>CreatedAt}||{Id,_,TopicId,CreatedAt}
+    <-dets:match_object(?USER_TOPIC_TABLE, {'_',UserId,'_','_'})],
+    Result.
+
+-spec check_if_subscribed(TopicId::domain:topic_id(),UserId::domain:user_id())->{ok,boolean()}|{error,Error::term()}.
 check_if_subscribed(TopicId,UserId)->
-    
-    Statement= <<"SELECT Count(user_id) FROM  user_topic WHERE topic_id = $1 AND user_id=$2">>,
-    {ok,C}=create_connection(),
-    {ok,_,[{Count}]}=epgsql:equery(C,Statement,[TopicId,UserId]),
-    {ok,Count>0}.
-
--spec does_topic_exist(TopicId::domain:topic_id())-> true | false | {error,Error::any()}.
-does_topic_exist(TopicId)->
-    Statement= <<"SELECT 1 FROM topic WHERE id = $1;">>,
-    {ok,C}=create_connection(),
-    {ok, Result} = epgsql:equery(C, Statement, [TopicId]),
-    % Check if the topic exists
-    Exists= length(Result)>0,
-    Exists.
+    Result=case dets:match_object(?USER_TOPIC_TABLE, {'_',UserId,TopicId,'_'}) of
+        []->false;
+        [_]-> true
+    end,
+    Result.
 
 
+    % id SERIAL PRIMARY KEY,
+    % topic_id INTEGER REFERENCES topic(id),
+    % user_id INTEGER REFERENCES wsuser(id),
+    % message VARCHAR(255),
+    % created_at TIMESTAMPTZ,
+    % timezone VARCHAR(255)
+-spec get_newest_messages(TopicId::domain:topic_id(),Count::number())->{ok,Messages::list()}|{error,Reason::term()}.
+get_newest_messages(TopicId,Count)->
+    Messages=case dets:match(?MESSAGE_TABLE,{'_',TopicId,'_','_','_','_'},Count) of
+                '$end_of_table' -> [];
+                 {error,Reason}->{error,Reason};
+                 Results->
+                    {ok,[
+                     #{<<"id">> => Id ,
+                     <<"user_id">>=> UserId ,
+                     <<"topic_id">>=> TopicId,
+                     <<"message">>=> Message,
+                     <<"created_at">>=> CreatedAt,
+                     <<"timezone">>=> Timezone}||{Id,UserId,_,Message,CreatedAt,Timezone}<-Results]}
+             end,
+    Messages.
 
 -spec get_oldest_messages(TopicId::domain:topic_id(),StartIndex::integer(),Count::integer())->{ok,Messages::list()}| {error,Error::term()}.
-
 get_oldest_messages(TopicId,StartIndex,Count)->
-    Statement="SELECT * FROM message WHERE topic_id = $1 AND id < $2 ORDER BY id DESC LIMIT $3;",
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[TopicId,StartIndex,Count]),
-    {ok,to_records(Columns, Values)}.
-
--spec get_newest_messages(TopicId::domain:topic_id(),Count::integer())->{ok,Messages::list()}| {error,Error::term()}.
-get_newest_messages(TopicId,Count)->
-    Statement="SELECT * FROM message WHERE topic_id = $1 ORDER BY id DESC LIMIT $2;",
-    {ok,C}=create_connection(),
-    {ok,Columns,Values}=epgsql:equery(C,Statement,[TopicId,Count]),
-    {ok,to_records(Columns, Values)}.
+    Pattern={'_',TopicId,'_','_','_','_'},
+    Results=dets:match_object(?MESSAGE_TABLE, Pattern),
+    FilteredMessages=lists:foldl(
+             fun({Id,UserId,_TopicId,Message,CreatedAt,Timezone},Acc)->
+                case {Id<StartIndex,length(Acc)<Count} of
+                    {true,true}->
+                        [#{<<"id">>=>Id,
+                           <<"user_id">>=>UserId,
+                           <<"topic_id">>=>TopicId,
+                           <<"message">>=>Message,
+                           <<"created_at">>=>CreatedAt,
+                           <<"timeezone">>=>Timezone}|Acc];
+                    _ ->Acc
+                end
+            end,
+            [],
+            Results),
+            {ok,lists:reverse(FilteredMessages)}.
 
 -spec write_chat_message(Message::domain:message())->ok | {error,Error::any()}.
 write_chat_message(Input=#message{user_id = UserId,topic_id = TopicId,content = Content,created_at = CreatedAt,timezone = Timezone})->
-
-    Statement= <<"INSERT INTO  message(topic_id,user_id,message,created_at,timezone) 
-                values ($1,$2,$3,$4,$5)">>,
-    io:format("\nInput is ~p\n",[Input]),
-    {ok,C}=create_connection(),
-    {ok,_}=case epgsql:equery(C,Statement,[TopicId,UserId,Content,CreatedAt,Timezone]) of
-             {ok,_} -> {ok,1};
-             {error,Reason}->io:format("\nError ~p",[Reason]),
-                             Reason
-           end,
-     ok.   
+    Id=erlang:unique_integer([monotonic,positive]),
+    Record={Id,UserId,TopicId,Content,CreatedAt,Timezone},
+    dets:insert(?MESSAGE_TABLE,Record),
+    ok.
 
 -spec write_chat_messages(Messages::[domain:message()])->{ok,Inserted::integer()} | {error,Error::any()}.
 write_chat_messages(Messages)->
-    Statement= <<"INSERT INTO  message(topic_id,user_id,content,createdAt,timezone) values ($1,$2,$3,$4)">>,
-    {ok,C}=create_connection(),
-    epgsql:transaction_start(C),
-    {ok,Stmt}=epgsql:prepare(C,"insert_statement",Statement),
-    ok=write_messages(C,Stmt, Messages),
-    ok.   
+    try
+    Records=[
+            {erlang:unique_integer([monotonic, positive]),
+             Msg#message.user_id,
+             Msg#message.topic_id,
+             Msg#message.content,
+             Msg#message.created_at,
+             Msg#message.timezone}||Msg<-Messages],
+    dets:insert(?MESSAGE_TABLE, Records),
+    ok
+        catch
+            _:Error->{error,Error}
+        end.
 
-write_messages(_Connection,_Stmt,[])->ok;
-write_messages(Connection,Stmt,[Message|Rest])->
-    #message{topic_id =TopicId,user_id =UserId,content=Content,
-      created_at=CreatedAt,timezone=Timezone}=Message,
-    epgsql:execute(Connection,Stmt,[TopicId,UserId,Content,CreatedAt,Timezone]),
-    write_messages(Connection, Stmt, Rest).
 
 
-
-to_records(_,[])->[];
-to_records(Columns,Values)->
-    to_records(Columns, Values,[]).
-to_records(_,[],Acc)->Acc;
-to_records(Columns,[CurrentRow|Rest],Acc)->
-    to_records(Columns,Rest, [to_map(Columns,tuple_to_list(CurrentRow),#{})|Acc]).
-
-to_map([],[],Acc)->Acc;
-to_map([Column|Columns],[Value|Values],Map)->
-    Key=lists:nth(2, tuple_to_list(Column)),
-    to_map(Columns,Values,Map#{Key=>Value}).
+  
